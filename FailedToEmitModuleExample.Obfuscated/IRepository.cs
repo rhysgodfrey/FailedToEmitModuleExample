4a28a1068eada7cb0d126cebdb7f57f69d5c63c8
Rhys Godfrey <rhys@ardourdigital.co.uk>
2015-08-11 09:30:40 +0100

    Failed to Emit Module Example

---
+++ FailedToEmitModuleExample.Obfuscated/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FailedToEmitModuleExample.Obfuscated
+{
+    public interface IRepository
+    {
+        Item Get(Guid contentId);
+
+        Item Get(int threadId, ItemGetOptions options = null);
+    }
+
+    public class Item
+    {
+    }
+
+    public class ItemGetOptions
+    {
+    }
+}
---
+++ FailedToEmitModuleExample/Tester.cs
@@ -0,0 +1,14 @@
+﻿using FailedToEmitModuleExample.Obfuscated;
+
+namespace FailedToEmitModuleExample
+{
+    public class Tester
+    {
+        private readonly IRepository _repository;
+
+        public void DoSomething()
+        {
+            var result = _repository.Get(123);
+        }
+    }
+}
